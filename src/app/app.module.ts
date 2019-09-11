ee2fce4f8179d5aeba79aab64c92f3bab305cdde
Matheus Zanin <matt_z6@hotmail.com>
2019-09-11 20:54:19 -0300

    Instalando Material & FlexLayout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+
+// Material & estilo
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MyOwnCustomMaterialModule } from "./material.module";
+import { FlexLayoutModule } from "@angular/flex-layout";
+
+// Páginas
+import { HomeComponent } from "./pages/home/home.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, HomeComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MyOwnCustomMaterialModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
